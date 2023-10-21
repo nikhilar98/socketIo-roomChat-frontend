c64890eea9eb5276904a133c2ea63272520f1ad9
nikhilar98 <nikhilar98@gmail.com>
2023-10-21 18:59:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState,useEffect } from "react";
+import {io} from 'socket.io-client'
+
+const socket = io.connect('http://localhost:3500')
+
+function App() {
+
+  const [message,setMessage] = useState("")
+  const [room,setRoom]= useState("")
+  const [receivedMsgs,setReceivedMsgs] = useState([])
+
+  // useEffect(()=>{
+  //   setReceivedMsgs([])
+  //   setMessage('')
+  // },[room])
+
+
+  function joinRoom(){
+    setReceivedMsgs([])
+    setMessage('')
+    socket.emit('joinroom',room)
+  }
+
+  function leaveRoom(){
+    setReceivedMsgs([])
+    setMessage('')
+    setRoom('')
+  }
+  
+  function sendMessage(e){
+      e.preventDefault()
+      socket.emit('send-message',{message,room})
+      setMessage("")
+  }
+
+  
+  socket.on('receive-message',(data)=>{
+        setReceivedMsgs([...receivedMsgs,data])
+  })
+  
+  console.log(socket.id)
+
+  return (
+    <div>
+        <fieldset style={{width:"400px",marginTop:"70px"}}>
+          <h1>Messages</h1> 
+          <input type="text" value={room} onChange={(e)=>{setRoom(e.target.value)}}/>
+          <button onClick={joinRoom}>join room</button> <button onClick={leaveRoom}>leave room</button>
+          <hr />
+          <ul style={{listStyle:"none"}}>
+            {
+              receivedMsgs.map((ele,i)=>{
+                return <li key={i} style={socket.id==ele.id ? {textAlign:'right'} : {textAlign:'left'}}><span class='message'>{ele.message}</span><br/><span class='time'>{new Date().toTimeString().slice(0,8)+','+new Date().toLocaleDateString()}</span></li>
+              })
+            }
+          </ul>
+          <form onSubmit={sendMessage}>
+            <input type="text" value={message} onChange={(e)=>{setMessage(e.target.value)}}/>
+            <input type='submit' value='send' disabled={!room}/>
+          </form>
+        </fieldset>
+    </div>
+  )
+}
+
+export default App;
