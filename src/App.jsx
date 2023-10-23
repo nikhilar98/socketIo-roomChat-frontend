f99c8e1f08904900ee064c1a25977702fdf46010
nikhilar98 <nikhilar98@gmail.com>
2023-10-23 18:57:05 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react";
+import { useState,useRef,useEffect} from "react";
 import {io} from 'socket.io-client'
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -12,38 +12,64 @@ function App() {
   const [room,setRoom]= useState("")
   const [name,setName] = useState("")
   const [enableSend,setEnableSend] = useState(false)
+  const [members,setMembers] = useState(0)
   const [receivedMsgs,setReceivedMsgs] = useState([])
+  const [formErrors,setFormErrors] = useState({})
+  const div = useRef(null)
 
   useEffect(()=>{
-    if(localStorage.getItem('chat')){
+      div.current.scrollIntoView({ behavior: "smooth"})
+  },[receivedMsgs])
 
+  const errors = {} 
+
+  const runValidations = () => { 
+    if(room===''){
+      errors.room = 'Room id is required.'
     }
-  },[])
+    if(name===''){
+      errors.name = 'name is required.'
+    }
+  }
 
   console.log(socket.id)
 
 
   function joinRoom(){
-    setEnableSend(true)
-    setReceivedMsgs([])
-    setMessage('')
-    socket.emit('joinroom',room)
+    runValidations() 
+    if(Object.keys(errors).length===0){
+      setFormErrors({})
+      setEnableSend(true)
+      setReceivedMsgs([])
+      setMessage('')
+      socket.emit('joinroom',room)
+    }
+    else { 
+      setFormErrors(errors)
+    }
   }
 
   function leaveRoom(){
+    socket.emit('leave-room',{room,name})
     setEnableSend(false)
     setReceivedMsgs([])
     setMessage('')
     setName('')
     setRoom('')
+    setMembers(0)
   }
   
   function sendMessage(e){
       e.preventDefault()
-      socket.emit('send-message',{message,room,name})
-      setMessage("")
+      if(message){
+        socket.emit('send-message',{message,room,name})
+        setMessage("")
+      }
   }
 
+  socket.on('receive-rooms',(data)=>{
+      setMembers(data)
+  })
   
   socket.on('receive-message',(data)=>{
         setReceivedMsgs([...receivedMsgs,data])
@@ -51,7 +77,7 @@ function App() {
 
   return (
     <div className="container">
-        <fieldset style={{width:"400px",marginTop:"70px"}}>
+        <fieldset>
           <h1>BlazeText
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-1 h-1" width="40px">
             <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
@@ -59,14 +85,14 @@ function App() {
             </svg>
           </h1> 
           <Box component="form" sx={{'& > :not(style)': { m: 1, width: '18ch' }}} noValidate autoComplete="on">
-              <TextField label="enter room number" variant="filled" type="number" value={room} onChange={(e)=>{setRoom(e.target.value)}} size="small" color="success"/>
-              <TextField label="enter your name" variant="filled" type="text" value={name} onChange={(e)=>{setName(e.target.value)}} size="small" color="success"/>
-          </Box><br/><br/>
+              <TextField error={Boolean(formErrors.room)} label={formErrors.room ? "Error":"enter room number"} variant="filled" type="number" value={room} onChange={(e)=>{setRoom(e.target.value)}} size="small" helperText={formErrors.room && formErrors.room}color="success"/>
+              <TextField error={Boolean(formErrors.name)} label={formErrors.name ?"Error":"enter your name"} variant="filled" type="text" value={name} onChange={(e)=>{setName(e.target.value)}} size="small" helperText={formErrors.name && formErrors.name} color="success"/>
+          </Box>{ Boolean(members) && <span> Members in room : <button className='count' disabled={true}>{members}</button> </span>}<br/><br/>
           <Button variant="contained" size="small" onClick={joinRoom} style={{backgroundColor:"green"}}>join</Button>
           <Button variant="contained" size="small" onClick={leaveRoom} style={{backgroundColor:"red",color:"black",marginLeft:"10px"}}>leave</Button>
           <hr />
-            <ul style={{listStyle:"none",padding:'0',overflow:'auto',height:"300px"}}>
-              {
+            <ul>
+              {  
                 receivedMsgs.map((ele,i)=>{
                   return <li key={i} style={socket.id===ele.id ? {textAlign:'right'} : {textAlign:'left'}}>
                       <span className='username'>{ele.name}</span><br/>
@@ -75,10 +101,11 @@ function App() {
                     </li>
                 })
               }
+              <div ref={div}></div>
             </ul>
           <div className="send-box"> 
             <form onSubmit={sendMessage}>
-                <TextField label="your message goes here..." variant="outlined" type="text" value={message} onChange={(e)=>{setMessage(e.target.value)}} size="small" />
+                <TextField label="your message goes here..." variant="outlined" type="text" value={message} onChange={(e)=>{setMessage(e.target.value)}} size="small" autoComplete="off"/>
 
                 <Button variant="contained" size="medium" type='submit' disabled={!(enableSend&&room&&name)} style={{marginLeft:"10px"}}>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
